579e28bd1775deef14167f85b326759b5ddf4048
Cooper-Higgins <jamie@supplantree.com>
2023-03-30 21:29:01 +0100

    Pass on football results, stuck on aggregating results

--- start_code/football_results/src/football_results.py
+++ start_code/football_results/src/football_results.py
@@ -1,6 +1,13 @@
+
+
 def get_result(final_score):
-    pass
+    if final_score["home_score"] > final_score["away_score"]:
+        return "Home win"
+    elif final_score["home_score"] < final_score["away_score"]:
+        return "Away win"
+    else: 
+        return "Draw"
 
 def get_results(final_scores):
-    pass
-    # (You could try and use a list comprehension for this)
+    final_scores = [get_result[0], get_result[1], get_result[2]]
+    return final_scores
--- start_code/football_results/tests/football_results_test.py
+++ start_code/football_results/tests/football_results_test.py
@@ -2,15 +2,42 @@ import unittest
 from src.football_results import *
 
 class FootballResultsTest(unittest.TestCase):
-    pass
-    # Test we get the right result string for a final score dictionary representing -
+    
+    def setUp(self):
+        self.final_score = [{
+            "home_score" : 3,
+            "away_score" : 1
+        },
+        {
+            "home_score" : 0,
+            "away_score" : 1
+        },
+        {
+            "home_score" : 2,
+            "away_score" : 2
+        }]
 
-        # Home win
-        # Away win
-        # Draw
+    # Test we get the right result string for a final score dictionary representing -
+    def test_get_result_home_win(self):
+        actual_result = get_result(self.final_score[0])
+        expected_result = "Home win"
+        self.assertEqual(expected_result, actual_result)
+
+    def test_get_result_away_win(self):
+        actual_result = get_result(self.final_score[1])
+        expected_result = "Away win"
+        self.assertEqual(expected_result, actual_result)
+
+    def test_get_result_draw(self):
+        actual_result = get_result(self.final_score[2])
+        expected_result = "Draw"
+        self.assertEqual(expected_result, actual_result)
 
     # Test we get right list of result strings for a list of final score dictionaries. 
-
+    def test_get_results(self):
+        actual_result = get_results(self.final_score)
+        expected_result = "Home win, Away win, Draw"
+        self.assertEqual(expected_result, actual_result)
 
 if __name__ == "__main__":
     unittest.main()
